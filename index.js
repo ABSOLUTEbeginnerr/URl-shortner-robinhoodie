9550c5a357c8feac6f1b819e055bf1be550f7062
ABSOLUTEbeginnerr <82446276+ABSOLUTEbeginnerr@users.noreply.github.com>
2024-03-14 13:10:06 +0200

    .

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
 require('dotenv').config();
+const validUrl = require('valid-url');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
-
-// Basic Configuration
 const port = process.env['PORT'] || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
-
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+function isValidUrl(url) {
+  const urlRegex = /^(http|https):\/\/[^ "]+$/;
+  return urlRegex.test(url);
+}
+
+let counter = 1;
+const shortUrls = {};
+
+
+
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+
+  if (isValidUrl(url)){
+    let short_url = counter++;
+
+    shortUrls[short_url] = url;
+
+    res.json({
+      original_url: url,
+      short_url: short_url
+    });
+
+    console.log(shortUrls);
+    }else{
+      res.json({error:'invalid url'})
+    }
+
+
 });
+
+app.get("/api/shorturl/:shorturl?", (req, res)=>{
+  const key = req.params.shorturl;
+  const orgUrl = shortUrls[key];
+  
+  res.redirect(orgUrl);
+
+})
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
